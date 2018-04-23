4d2f8ed337ff4b2bf9dfbaaa8ae785ab5a5057ab
nhNet <38656592+nhNet@users.noreply.github.com>
2018-04-23 19:47:17 +0200

    Update Ad.js

--- js/Ad.js
+++ js/Ad.js
@@ -1,19 +1,21 @@
 //Code for Ad!
 //Made by Nicholas Huaman
-    var num1=800;
-    var num2=700;
-    var fnum1=40;
-    var fnum2=65;
-    setInterval(scaleDownLogo, 1);
-    
-    function scaleDownLogo(){
-        num1--;
-      num2--;
-        document.getElementById("Ad").style.backgroundSize = num1 + "px " + num2 + "px";
-      if (num1==0){scaleDownText();}
-    }
-    function scaleDownText(){
-        document.getElementById("Ad").innerHTML="<br><br><br><br><br><br><br><br><br><br><p style='user-select:none;font-size:"+fnum1+"px;'>Made by the creator of</p><h1 style='user-select:none;font-size:"+fnum2+"px'>HuamanGames</h1>";
+    function Ad(){
+        var num1=800;
+        var num2=700;
+        var fnum1=40;
+        var fnum2=65;
+        setInterval(scaleDownLogo, 1);
+
+        function scaleDownLogo(){
+            num1--;
+          num2--;
+            document.getElementById("Ad").style.backgroundSize = num1 + "px " + num2 + "px";
+          if (num1==0){scaleDownText();}
+        }
+        function scaleDownText(){
+            document.getElementById("Ad").innerHTML="<br><br><br><br><br><br><br><br><br><br><p style='user-select:none;font-size:"+fnum1+"px;'>Made by the creator of</p><h1 style='user-select:none;font-size:"+fnum2+"px'>HuamanGames</h1>";
+        }
     }
     function FinishAd(){
         document.body.style.background="white";
@@ -25,3 +27,4 @@
         document.body.style.color="black";
         document.body.innerHTML='<span style="user-select:none;"><h1 style="font-size:50px;">NH Twitch</h1><br><br><p>This page allows you to view live twitch streams without it saving in your history or on your network. All you shall see there is this website! :)</p><p>WARNING: Extensions (Like Omnito and Netop) can see this... <br><br><p>Press the keys alt+H to search without pressing the button. (More discreet)</p><p>If you' + " don't" +' understand Attributes go to this link here on <a href="https://www.w3schools.com/tags/tag_iframe.asp">W3 Attributes for Iframes...</a></p><br><br><span style="user-select:none;"><div>Twitch Streamer:<input style="width:30%;" class="Box" placeholder="eg. Ninja" type="text"></div><div>Add any attributes (Options):<input placeholder="eg. allowfullscreen=(quotes here)true(quotes here)" style="width:17%;" class="Box2" type="text"><button accesskey="h" onclick="SUBMITtwitch();">Submit Chromebook Fullscreen</button></span>'
     }
+Ad();
