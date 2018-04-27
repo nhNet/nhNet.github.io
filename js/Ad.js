6b1c80b9858e08021c80c6f0c43c88ee8119f9fb
Browsers <38656592+nhNet@users.noreply.github.com>
2018-04-27 18:09:19 +0200

    Update Ad.js

--- js/Ad.js
+++ js/Ad.js
@@ -14,6 +14,113 @@ addEventListener(
     },
     10
 );
+
+
+
+
+
+
+
+
+
+var Cookies = {
+
+  /* Sets a cookie. The parameters are:
+   *
+   * name    - the name of the cookie
+   * value   - the value of the cookie
+   * options - an optional object which may have the following keys:
+   *           domain - the domain for which the cookie should be set; if
+   *                    omitted, the cookie is set for the current domain
+   *           path   - the path for which the cookie should be set; if omitted,
+   *                    the cookie is set for the current path
+   *           expiry - the expiry date, as either an instance of Date or a
+   *                    number of seconds; if omitted, the cookie expires at the
+   *                    end of the browser session
+   *           secure - true if the cookie should only be sent over encrypted
+   *                    connections; if omitted or false, the cookie is sent
+   *                    over both encrypted and unencrypted connections
+   */
+  set : function(name, value, options){
+
+    // initialise the cookie data
+    var data = [encodeURIComponent(name) + '=' + encodeURIComponent(value)];
+
+    // check whether there are nay options
+    if (options){
+
+      // extend the data with the expiry date if necessary
+      if ('expiry' in options){
+        if (typeof options.expiry == 'number'){
+          options.expiry = new Date(options.expiry * 1000 + +new Date);
+        }
+        data.push('expires=' + options.expiry.toGMTString());
+      }
+
+      // extend the data with the other options if necessary
+      if ('domain' in options) data.push('domain=' + options.domain);
+      if ('path'   in options) data.push('path='   + options.path);
+      if ('secure' in options && options.secure) data.push('secure');
+
+    }
+
+    // set the cookie
+    document.cookie = data.join('; ');
+
+  },
+
+  /* Returns the value of the specified cookie. If keepDuplicates is not set or
+   * false then the return value is a string, or the value undefined if there is
+   * no cookie with the specified name. If keepDuplucates is true then the
+   * return value is an array of strings, which will be empty if there are no
+   * cookies with the specified name. The parameters are:
+   *
+   * name           - the name of the cookie
+   * keepDuplicates - true to return an array containing the values of all
+   *                  cookies with the specified name; if omitted or false a
+   *                  string (or the value undefined) will be returned
+   */
+  get : function(name, keepDuplicates){
+
+    // initialise the list of values
+    var values = [];
+
+    // loop over the cookies
+    var cookies = document.cookie.split(/; */);
+    for (var i = 0; i < cookies.length; i ++){
+
+      // if the cookie has the requested name, add its value to the list
+      var details = cookies[i].split('=');
+      if (details[0] == encodeURIComponent(name)){
+        values.push(decodeURIComponent(details[1].replace(/\+/g, '%20')));
+      }
+
+    }
+
+    // return the first value or all values as appropriate
+    return (keepDuplicates ? values : values[0]);
+
+  },
+
+  /* Clears a cookie. The parameters are:
+   *
+   * name    - the name of the cookie
+   * options - an optional object which may have the following keys:
+   *           domain - the domain for which the cookie was set; if omitted, the
+   *                    cookie is cleared for the current domain
+   *           path   - the path for which the cookie was set; if omitted, the
+   *                    cookie is cleared for the current path
+   */
+  clear : function(name, options){
+
+    // clear the cookie
+    if (!options) options = {};
+    options.expiry = -86400;
+    this.set(name, '', options);
+
+  }
+
+};
     function Resize(){
         document.body.style.background="black";
         document.body.style.color="white";
@@ -44,7 +151,7 @@ addEventListener(
     function FinishAdWithTwitch(){
         document.body.style.background="black";
         document.body.style.color="black";
-        document.body.innerHTML='<span style="user-select:none;"><div style="background:url(https://upload.wikimedia.org/wikipedia/commons/f/f4/Stoneseamless.png);color:white;margin-left:-7.51565px;margin-top:-7.51565px;border:5px solid black;width:100.38%;"><h1 style="font-size:50px;"><img style="border-radius:10px;background:white;margin:5px;" width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Twitch_mit_Glitch.png"> NH Twitch Browser</h1></div><div style="margin-left:-7.51565px;height:400px;margin-top:-3px;color:white;width:30%;font-size:45px;border:3px solid black;background:url(https://www.publicdomainpictures.net/pictures/120000/velka/abstract-colorful-seamless-pattern.jpg);background-size:300px 270px;color:white;font-family: '+"'Gamja Flower'"+', cursive;font-weight:bolder;padding:20px;"><div style="text-align:left;"><div style="border-radius:30px;background:black;">Twitch Streamer<span style="color:red;"><b><u>*</u></b></span>:</div><input style="width:90%;border-radius:25px;font-size:40px;" class="Box" placeholder="eg. Ninja" type="text"></div><br><div style="text-align:left;"><div style="border-radius:30px;background:black;">Add any attributes (Optional):</div><input placeholder="eg. style=&quot;HERE CSS&quot;" style="height:50px;width:90%;border-radius:300px;font-size:20px;" class="Box2" type="text"></div></div><div style="margin-top:-444px;margin-left:443px;width:49.04%;background:url(http://www.cadhatch.com/communities/4/004/013/003/604//images/4618103428_100x100.jpg);background-size:300px 300px;border:3px solid black;font-weight:bolder;padding:120px;"><button style="font-size:50px;background:url(http://www.cadhatch.com/communities/4/004/013/003/604//images/4618103426_100x100.jpg);border-radius:300px;font-size:60px;font-family: '+"'Gamja Flower'"+', cursive;color:white;" accesskey="h" onclick="SUBMITtwitch();">Submit Chromebook Fullscreen</button></div></span><div style="text-align:center;background:#efdd97;height:166px;margin-left:-7.51565px;width:100.38%;border:5px solid black;"><h1>If you do not understand this website, (including: its purposes, how to use it and how it is done) please '+ "don't"+' hesitate to ask the developer of this website any questions at <a href="mailto:spbong999@gmail.com">this email address.</a></h1><h1>If any further questions need answering just check out the <a href="https://nhnet.github.io/info/"> info page.</a></h1></div>';
+        document.body.innerHTML='<span style="user-select:none;"><div style="background:url(https://upload.wikimedia.org/wikipedia/commons/f/f4/Stoneseamless.png);color:white;margin-left:-7.51565px;margin-top:-7.51565px;border:5px solid black;width:100.38%;"><h1 style="font-size:50px;"><img style="border-radius:10px;background:white;margin:5px;" width="50px" height="50px" src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Twitch_mit_Glitch.png"> NH Twitch Browser</h1></div><div style="margin-left:-7.51565px;height:400px;margin-top:-3px;color:white;width:30%;font-size:45px;border:3px solid black;background:url(https://www.publicdomainpictures.net/pictures/120000/velka/abstract-colorful-seamless-pattern.jpg);background-size:300px 270px;color:white;font-family: '+"'Gamja Flower'"+', cursive;font-weight:bolder;padding:20px;"><div style="text-align:left;"><div style="border-radius:30px;background:black;">Twitch Streamer<span style="color:red;"><b><u>*</u></b></span>:</div><input style="width:90%;border-radius:25px;font-size:40px;" class="Box" autocomplete="on" name="inputStreamer" placeholder="eg. Ninja" type="text"></div><br><div style="text-align:left;"><div style="border-radius:30px;background:black;">Add any attributes (Optional):</div><input placeholder="eg. style=&quot;HERE CSS&quot;" style="height:50px;width:90%;border-radius:300px;font-size:20px;" class="Box2" type="text"></div></div><div style="margin-top:-444px;margin-left:443px;width:49.04%;background:url(http://www.cadhatch.com/communities/4/004/013/003/604//images/4618103428_100x100.jpg);background-size:300px 300px;border:3px solid black;font-weight:bolder;padding:120px;"><button style="font-size:50px;background:url(http://www.cadhatch.com/communities/4/004/013/003/604//images/4618103426_100x100.jpg);border-radius:300px;font-size:60px;font-family: '+"'Gamja Flower'"+', cursive;color:white;" accesskey="h" onclick="SUBMITtwitch();">Submit Chromebook Fullscreen</button></div></span><div style="text-align:center;background:#efdd97;height:166px;margin-left:-7.51565px;width:100.38%;border:5px solid black;"><h1>If you do not understand this website, (including: its purposes, how to use it and how it is done) please '+ "don't"+' hesitate to ask the developer of this website any questions at <a href="mailto:spbong999@gmail.com">this email address.</a></h1><h1>If any further questions need answering just check out the <a href="https://nhnet.github.io/info/"> info page.</a></h1></div>';
     }
     function FinishAdWithInfo(){
         document.body.innerHTML='<span style="user-select:none;"><h1>NH Youtube Browser</h1><br><br><p>This page allows you to view youtube videos without it saving in your history or on your network. All you shall see there is this website! :)</p><p>Please note: Extensions (Like Omnito and Netop) can see this... <br><p>The YouTube video ID is what YouTube uses to have a link to store for each unique video. This has to have been searched for beforehand. (In the google part of the browser)</p><p>Some videos contain sound that does not allow itself to be embedded. This can result in you not being able to watch it. Search for that same video in the google browser.</p><br><p>Press the keys alt+H to search without pressing the button. (More discreet)</p><br><p>NH YouTube Browser <b> is allowed by YouTube</b> to embed their videos.</p><br><h1>NH Twitch</h1><span style="user-select:none;"><br><br><p>This page allows you to view live twitch streams without it saving in your history or on your network. All you shall see there is this website! :)</p><p>Please note: Extensions (Like Omnito and Netop) can see this... <br><br><p>Press the keys alt+H to search without pressing the button. (More discreet)</p><p>If you' + " don't" +' understand Attributes go to this link here on <a href="https://www.w3schools.com/tags/tag_iframe.asp">W3 Attributes for Iframes...</a></p><br><br><br><p>NH Twitch Browser <b> is allowed by Twitch</b> to play their streams and does <b>not</b> restrict the streamers from earning their daily wage.</p><p><span style="color:red;"><b><u>*</u></b></span>Make sure you know a few in case some are offline!</p>';
