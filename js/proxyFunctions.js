5664a18bc54e4a8c47b63028a2de1292511dd318
Browsers <38656592+nhNet@users.noreply.github.com>
2018-04-27 15:46:02 +0200

    Update proxyFunctions.js

--- js/proxyFunctions.js
+++ js/proxyFunctions.js
@@ -7,7 +7,7 @@ function SUBMITyoutube(){
 function OpenTwitch(streamer, attr){
     document.body.style.background="url(http://www.solidbackgrounds.com/images/950x350/950x350-black-solid-color-background.jpg) no-repeat fixed top";
     document.body.style.backgroundSize="100% 5.5%";
-    document.body.innerHTML='<div><input style="width:20%;" class="Box" placeholder="Streamer: eg. Ninja" value="'+streamer+'" type="text"><input placeholder="Attributes: eg. style=&quot;&quot;" value="'+attr+'" style="width:30%;" class="Box2" type="text"><button accesskey="h" onclick="SUBMITtwitch();">Submit Chromebook Fullscreen</button><br><br><iframe frameborder="0" style="margin:-7.6px;" width="101.2%" allowfullscreen="true" height="725.5px" src="https://player.twitch.tv/?channel=' + streamer + '"' + attr + '>Your Browser does not support Iframes. Get a new one!</iframe>';
+    document.body.innerHTML='<div><input style="width:20%;" class="Box" placeholder="Streamer: eg. Ninja" value="'+streamer+'" type="text"><input placeholder="Attributes: eg. style=&quot;&quot;" value="'+attr+'" style="width:30%;" class="Box2" type="text"><button accesskey="h" onclick="SUBMITtwitch();">Submit Chromebook Fullscreen</button><br><br><iframe frameborder="0" style="margin:-7.6px;" width="76.2%" allowfullscreen="true" height="725.5px" src="https://player.twitch.tv/?channel=' + streamer + '"' + attr + '>Your Browser does not support Iframes. Get a new one!</iframe><iframe src="https://www.twitch.tv/embed/dakotaz/chat" width="25%" height="725.5px" frameborder="0"></iframe>';
 }
 function OpenYouTube(id, attr){
     document.body.style.background="url(http://www.solidbackgrounds.com/images/950x350/950x350-black-solid-color-background.jpg) no-repeat fixed top";
