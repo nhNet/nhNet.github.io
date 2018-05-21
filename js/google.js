2cbc4d635efd3ae4dfb26ea54e6cd52e16f1ab5f
Browsers <38656592+nhNet@users.noreply.github.com>
2018-05-21 13:30:02 +0200

    Update google.js

--- js/google.js
+++ js/google.js
@@ -1,11 +1,6 @@
 // Nicholas Huaman 2018
 
-// Declare all account names by their email.
-var allAccounts = ["spbong999@gmail.com","nicholashuaman@gmail.com","nicholas.hua@kcpupils.org","pedrotomas.cob.agu@kcpupils.org","victor.arr.mar@kcpupils.org","patrickthomas.sch@kcpupils.org","lucasgabriel.sal.bev@kcpupils.org","antonio.sie.per@kcpupils.org","jorgegabriel.jof.vic@kcpupils.org","nathan.wal.san@kcpupils.org","gonzalo.agu.lag@kcpupils.org","ahmed.fte@kcpupils.org","rodrigo.par.rob@kcpupils.org","nicolas.arr.gra@kcpupils.org","juanpablo.bis@kcpupils.org","alonso.cas@kcpupils.org","david.eze.zam@kcpupils.org","diego.mor.man@kcpupils.org","andrew.row@kcpupils.org","beatriz.tay.rod@kcpupils.org","constanza.par.hor@kcpupils.org","daniel.mar.her@kcpupils.org","pablo.sai.mon@kcpupils.org","borja.alf.vic@kcpupils.org","matan.kes@kcpupils.org","marts.vec@kcpupils.org","marcos.car.val@kcpupils.org","victor.arr.mar@kcpupils.org","mrpekka740@gmail.com","rodrigo.alv.bor@kcpupils.org"];
-
-// This shall distinguish if the account is pro, or not.
-// If you name all the Pro accounts the only ones remaining will be the basic. Get it?
-var proAccounts = ["spbong999@gmail.com","antonio.sie.per@kcpupils.org","lucasgabriel.sal.bev@kcpupils.org","nathan.wal.san@kcpupils.org","jorgegabriel.jof.vic@kcpupils.org","daniel.mar.her@kcpupils.org", "constanza.par.hor@kcpupils.org", "beatriz.tay.rod@kcpupils.org", "gonzalo.agu.lag@kcpupils.org", "ahmed.fte@kcpupils.org", "pedrotomas.cob.agu@kcpupils.org", "juanpablo.bis@kcpupils.org", "nicolas.arr.gra@kcpupils.org","victor.arr.mar@kcpupils.org","rodrigo.par.rob@kcpupils.org","nicholas.hua@kcpupils.org","pablo.sai.mon@kcpupils.org","patrickthomas.sch@kcpupils.org","mrpekka740@gmail.com","andrew.row@kcpupils.org","borja.alf.vic@kcpupils.org","diego.mor.man@kcpupils.org","alonso.cas@kcpupils.org","david.eze.zam@kcpupils.org"];
+// Accounts defined at at https://nhnet.github.io/js/NH_Net_Accounts/private/seriously.you.shouldnt.be.here/accounts.js
 
 // If someone didn't complete the sign-in correctly, then something ought to happen!
 // This code is then executed.
