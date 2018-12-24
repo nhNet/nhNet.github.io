016b9853fcda897ebd44ede683a2f6df8e429455
Browsers <38656592+nhNet@users.noreply.github.com>
2018-12-24 10:28:16 +0100

    Add files via upload

---
+++ youtube/js/google.js
@@ -0,0 +1,73 @@
+// Nicholas Huaman 2018
+// First load all Google's scripts so that the signIn actually works!
+makeMeta("google-signin-scope","profile email");
+makeMeta("google-signin-client_id","398993796104-lq9k21a411mnehe5p94brocp3rs72dr5.apps.googleusercontent.com");
+loadScript("https://nhnet.github.io/management/private/Accounts/accounts.js");
+loadScript("https://apis.google.com/js/platform.js");
+ // Accounts defined at at https://nhnet.github.io/js/NH_Net_Accounts/private/seriously.you.shouldnt.be.here/accounts.js
+ // If someone didn't complete the sign-in correctly, then something ought to happen!
+// This code is then executed.
+function onFail(){
+    document.querySelector('#yt-logo').innerHTML='<img width="55" src="https://s.ytimg.com/yts/img/favicon-vfl8qSV2F.ico"> You closed the signin tab :(';
+}
+// I haven't really tried out this one.
+// It is supposed to sign u out and then complete your own functions
+function signOut() {
+    // Here is Google Signing you out
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        window.location.reload();
+    });
+}
+// Here is what happens when Google Signs u in correctly.
+function onSignIn(googleUser) {
+    var OnOff='off';
+    document.querySelector('#yt-logo').innerHTML='<img width="55" src="https://s.ytimg.com/yts/img/favicon-vfl8qSV2F.ico"> NH Youtube Search';
+    // Refer to Google using this variable. This is the base for the useful variables for code users
+    var profile = googleUser.getBasicProfile();
+    // Name  Google User Variables for future use!
+    var profileImg = profile.getImageUrl();
+    var profileName = profile.getName();
+    var profileEmail = profile.getEmail();
+    document.querySelector('#query').style.display='block';
+    document.querySelector('#subB').style.display='block';
+    document.querySelector('.g-signin2').style.display='none';
+    document.querySelector('#query').style.marginLeft='41%';
+    document.querySelector('#subB').style.marginLeft='56.9%';
+    document.querySelector('#subB').style.marginTop='-2.3%';
+    document.querySelector('.stuff-top-right').innerHTML="<div id='box'><h1 style='text-align:right'><span id='img'></span>&nbsp;<br><div id='name'></div></h1></div>";
+    document.getElementById('img').innerHTML="<img style='border-radius:15px' width='30px' height='30px' src='"+profileImg+"'> <img width='10px' src='https://image.flaticon.com/icons/svg/60/60995.svg'/>";
+    document.getElementById('img').onclick=function(){
+       if(OnOff=='off'){
+          OnOff='on';
+          document.getElementById('name').innerHTML= '<p style="margin-top:5px;" id="boop-box">Welcome, '+profileName.split(' ')[0]+'!<br><a href="javascript:signOut();">Sign out</a></p>';
+       }else{
+          OnOff='off';
+          document.getElementById('name').innerHTML= '';
+       }
+    };
+}
+ // This is unimportant.
+// It is what loads everything at the top!
+function loadScript(url) {
+    var script = document.createElement("script");
+    script.src = url;
+    script.async="true";
+    script.defer="true";
+     document.head.appendChild(script);
+}
+function makeMeta(name, content) {
+    var meta = document.createElement("meta");
+    meta.name = name;
+    meta.content = content;
+     document.head.appendChild(meta);
+}
+function makeStyle(content) {
+    var style = document.createElement("style");
+    style.innerHTML = content;
+    document.body.appendChild(style);
+}
+ // That's all folks!
+// Thank you for looking at my code.
+// It might even be of some use to you!
+// Nicholas Huaman 2018
---
+++ youtube/js/yt-code.js
@@ -0,0 +1,122 @@
+//Look @url!
+function queeries(stuff){
+    try{
+        var variable=window.location.toString();
+        variable=variable.split('?');
+        variable.shift();
+        try{
+            variable=variable[0].split('&').join(',');
+        }catch(err){
+            //
+        }
+        variable=variable.split('=').join(',');
+        variable=variable.split(',');
+        for(var i=0;i<stuff.length;i++){
+            var workingOn=stuff[i];
+            workingOn=workingOn.split('_');
+            if(variable.includes(workingOn[0])==true&&variable[variable.indexOf(workingOn[0])+1]==workingOn[1]){
+                eval(stuff[i+1]);
+            }
+            i++;
+        }
+    }catch(err){
+      //   alert(err)
+    }
+}
+//When Page ready...
+$(document).ready(function () {
+    var fromMus=false;
+    var download=false;
+    var times=0;
+    queeries(['from_Mus','fromMus=true;','download_true','download=true;']);
+    document.querySelectorAll('#closeBTN')[0].onclick=function(){document.querySelector('.modal-body').innerHTML='';};
+    document.querySelectorAll('#closeBTN')[1].onclick=function(){document.querySelector('.modal-body').innerHTML='';};
+    $('#search-term').submit(function (event) {
+        event.preventDefault();
+        if(times==0){
+            document.querySelector('form').style.animation='moveLogo 2s';
+            document.querySelector('.inps').style.animation='moveSearch 2s';
+            var searchTerm = $('#query').val();
+            getRequest(searchTerm);
+            var time=setTimeout(function(){
+                document.querySelector('form').style.animation='';
+                document.querySelector('form').style.marginTop='-5%';
+                document.querySelector('form').style.marginLeft='-50%';
+                document.querySelector('form').style.position='sticky';
+                document.querySelector('form').style.top='2px';
+                document.querySelector('.inps').style.animation='';
+                document.querySelector('.inps').style.marginTop='0%';
+                document.querySelector('.inps').style.marginLeft='50%';
+                document.querySelector('.inps').style.position='sticky';
+                document.querySelector('.inps').style.top='5px';
+                window.clearTimeout(time)
+            },2000);
+            times++;
+        }else{
+            var searchTerm = $('#query').val();
+            getRequest(searchTerm);
+        }
+    });
+});
+
+function getRequest(searchTerm) {
+    var url = 'https://www.googleapis.com/youtube/v3/search';
+    var params = {
+        part: 'snippet',
+        key: 'AIzaSyAN15Bt4fSc8TcsdaxUm0i52PCRwi-H_Bc',
+        q: searchTerm
+    };
+
+    $.getJSON(url, params, showResults);
+}
+
+function showResults(results) {
+    var html = "";
+    var entries = results.items;
+    $.each(entries, function (index, value) {
+        if(JSON.stringify(value.id.kind)=='"youtube#video"'){
+            var title = value.snippet.title;
+            var thumbnail = value.snippet.thumbnails.default.url;
+            html += '<div class="results">';
+            html += '<h3>' + title + '</h3>';
+            html += '<img style="border-radius:15px" draggable="false" src="' + thumbnail + '">';
+            html += `<br><br><button type="button" class="btn btn-info btn-lg" onclick="changeModal('video','`+value.id.videoId+`')" data-toggle="modal" data-target="#myModal">Open Video Tools</button>`;
+            html += '</div><br>';
+        }else if(JSON.stringify(value.id.kind)=='"youtube#channel"'){
+              var title = 'Channel: '+value.snippet.title;
+              var thumbnail = value.snippet.thumbnails.default.url;
+              html += '<div class="results">';
+              html += '<h1>' + title + '</h1>';
+              html += '<img style="border-radius:15px" draggable="false" src="' + thumbnail + '">';
+              html += `<br><br><button type="button" class="btn btn-info btn-lg" onclick="changeModal('channel','`+value.id.channelId+`')" data-toggle="modal" data-target="#myModal">Channel ID</button>`;
+              html += '</div><br>';
+      }
+    }); 
+
+    $('#search-results').html(html);
+}
+
+function changeModal(type, value){
+  try{
+    if(type=='channel'){
+        document.querySelector('.modal-body').innerHTML='Channel ID: '+value;
+    }else if(type=='video'){
+        document.querySelector('.modal-body').innerHTML='Video ID: '+value+'<br><iframe frameborder="0" src="https://www.youtube.com/embed/'+value+'" allowfullscreen sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts"></iframe>';
+        document.querySelector('.modal-footer').innerHTML='<button type="button" id="closeBTN" class="btn btn-default" data-dismiss="modal">Close</button>';
+        if(fromMus===true){
+              document.querySelector('.modal-footer').innerHTML+='<button type="button" id="vidIDtoUse" data-vidID="'+value+'" class="btn btn-primary" data-dismiss="modal">Get audio</button>';
+              document.querySelector('#vidIDtoUse').onclick=function(){
+                window.location='https://nhnet.github.io/Music/yt-audio.html?vidID='+document.querySelector('#vidIDtoUse').dataset.vidID;
+              };
+      }
+      if(download===true){
+              document.querySelector('.modal-footer').innerHTML+='<button type="button" id="downloadVid" data-vidID="'+value+'" class="btn btn-info" data-dismiss="modal">Download Video</button>';
+              document.querySelector('#downloadVid').onclick=function(){
+                  alert('Sorry, function not available yet...')
+              };
+      }
+    }
+  }catch(err){
+//       alert(err);
+  }
+}
